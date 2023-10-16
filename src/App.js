d574be9f0383bff8efdb4cbe48c97efacb41cf38
salah <salahsaamir707@gmail.com>
2023-10-16 19:16:35 +0300

    cors orgin

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { DbConnect } from "../db/Dbconnected.js";
 import { auth } from "./models/Auth/auth.router.js";
 import { GlobalError } from "./utilits/handleError.js";
-
+import cors from'cors'
 export const App = (express) => {
   const app = express();
   app.use(express.json())
+  app.use(cors())
   app.get("/", (req, res) => res.send("Hello World!"));
   app.use('/auth',auth)
   app.all("*", (req, res) => res.send(" router not found !"));
